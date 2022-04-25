b80c459500d224349e88b4c3a9c2b54d7219cf58
MostafaMohammadi1989 <Mostafa.mohammadi.1989.10.26@gmail.com>
2022-04-25 06:21:10 +0430

    Scrolling smooth for other browser and safari

--- js/script.js
+++ js/script.js
@@ -11,3 +11,32 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+/////////////////////////////////////////////////////////////
+///Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    ////////Scroll top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    ////////Scroll to other link
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    ////////Close navigation mobile
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
