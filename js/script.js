38c9f5b78958425441acf94bc0539366868ccc84
MostafaMohammadi1989 <Mostafa.mohammadi.1989.10.26@gmail.com>
2022-04-25 05:10:56 +0430

    make mobile navigation

---
+++ js/script.js
@@ -0,0 +1,13 @@
+///////////////////Set curent year //////////////////
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////Make Mobile Navigation //////////////////
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
